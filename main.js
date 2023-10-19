213222ecea99dee5b77221f721d4d67e09450441
Dhwanirnair <133526271+Dhwanirnair@users.noreply.github.com>
2023-10-18 19:14:32 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,61 @@
-
-
 canvas = document.getElementById("myCanvas");
-color = "red"; 
+ctx= canvas.getContext("2d");
 
-ctx = canvas.getContext("2d");
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "grey".
+    Use lineWidth and set the width to 1.
+    Use rect() to create a rectangle at 150 and 143
+with 430 width and 200 height.
+*/
+
 ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
+ctx.rect(150 ,143, 430, 200);
 ctx.stroke();
 
-canvas.addEventListener("mousedown", my_mousedown);
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "blue".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x = 250 and y = 210
+with 40 as radius, 0 startAngle and 2 * Math.PI endAngle.
+*/
 
-function my_mousedown(e)
-{
-    mouse_x = e.clientX - canvas.offsetLeft;
-    mouse_y = e.clientY - canvas.offsetTop;  
-    
-    console.log("X = " + mouse_x + " ,Y =  " + mouse_y);
-    circle(mouse_x , mouse_y);
-
-}
-
-
-function circle(mouse_x , mouse_y)
-{
-ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-ctx.arc(mouse_x , mouse_y , 40 , 0 , 2*Math.PI);
+ctx. beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250,210, 40, 0, 2 * Math.PI);
 ctx.stroke();
-}
 
+// Similarly, create a black circle with position 350 and 210
+ctx. beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(350,210, 40, 0 , 2 * Math.PI);
+ctx.stroke();
 
+// Similarly, create a red circle with position 210 and 450
 
+ctx. beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(450,210, 40, 0 , 2 * Math.PI);
+ctx.stroke();
 
-	
+// Similarly, create an orange circle with position 300 and 250
+
+ctx. beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 5;
+ctx.arc(300,250, 40, 0 , 2 * Math.PI);
+ctx.stroke();
+
+// Similarly, create a green circle with position 400 and 250
+
+ctx. beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(400,250, 40, 0 , 2 * Math.PI);
+ctx.stroke();
